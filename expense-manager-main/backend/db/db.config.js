62452f17a87bd4eac3e59d480c64d2d4226c738b
LavdhiJ <lavdhij@gmail.com>
2025-07-31 20:45:23 +0530

    Refactor: moved thr url out

--- expense-manager-main/backend/db/db.config.js
+++ expense-manager-main/backend/db/db.config.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 
 async function dbConnect(){
     
-    const connectionString = "mongodb+srv://lavdhij:4icRjwKPucihb4dY@tracker-expense.lvh4btb.mongodb.net/tracker-expense?retryWrites=true&w=majority"
+  
+    
     try { 
         await mongoose.connect(connectionString)
         console.log("Database Connected.");
